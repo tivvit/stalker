9d38b8e559ef5e63e01b28ed7cb426d2ee706ecf
Vit Listik <tivvit@seznam.cz>
2018-04-04 20:55:56 +0200

    store errors, fix systemd script

--- stalker_linux.sh
+++ stalker_linux.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+export DISPLAY=':0.0'
+
 while true; do
   FILE=$HOME/stalker/`date +%y-%m-%d`.log
-  touch $FILE
-  TITLE=`xdotool getactivewindow getwindowname`
-  PROC=`ps -fp $(xdotool getactivewindow getwindowpid) -o cmd=`
+  ERR_FILE=$HOME/stalker/`date +%y-%m-%d`.log
+  TITLE=`xdotool getactivewindow getwindowname 2>> $ERR_FILE`
+  PROC=`ps -fp $(xdotool getactivewindow getwindowpid) -o cmd= 2>> $ERR_FILE`
   echo "{\"timestamp\": `date +%s.%N`, \"title\": \"$TITLE\", \"proc\":\"$PROC\"}" >> $FILE
   sleep 0.5
 done
