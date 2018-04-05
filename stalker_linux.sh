f0d327cc7649b80d1cfa886027c4bb2b068df0d9
Vit Listik <tivvit@seznam.cz>
2018-04-06 01:55:58 +0200

    append pc name

--- stalker_linux.sh
+++ stalker_linux.sh
@@ -2,9 +2,10 @@
 
 export DISPLAY=':0.0'
 
+PC="pc"
 while true; do
-  FILE=$HOME/stalker/`date +%y-%m-%d`.log
-  ERR_FILE=$HOME/stalker/`date +%y-%m-%d`.err.log
+  FILE=$HOME/stalker/`date +%y-%m-%d`.${PC}.log
+  ERR_FILE=$HOME/stalker/`date +%y-%m-%d`.${PC}.err.log
   TITLE=`xdotool getactivewindow getwindowname 2>> $ERR_FILE`
   PROC=`ps -fp $(xdotool getactivewindow getwindowpid) -o cmd= 2>> $ERR_FILE`
   echo "{\"timestamp\": `date +%s.%N`, \"title\": \"$TITLE\", \"proc\":\"$PROC\"}" >> ${FILE}
