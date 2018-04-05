70d25b4cc783c20b26dc9e0bfc5a8c64b5d78c36
Vit Listik <tivvit@seznam.cz>
2018-04-06 01:51:00 +0200

    mac stalker with launch deamon

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Mac
+* `cp org.tivvit.stalker.plist ~/Library/LaunchAgents/`
+* `launchctl unload -w ~/Library/LaunchAgents/org.tivvit.stalker.plist`
+* `launchctl load -w ~/Library/LaunchAgents/org.tivvit.stalker.plist`
--- stalker_mac.sh
+++ stalker_mac.sh
@@ -4,6 +4,6 @@ PC="ntb"
 while true; do
   FILE=$HOME/stalker/`date +%y-%m-%d`.${PC}.log
   ERR_FILE=$HOME/stalker/`date +%y-%m-%d`.${PC}.err.log
-  ./active-win >> ${FILE} 2> ${ERR_FILE}
+  /Users/vit.listik/git/stalker/active-win >> ${FILE} 2> ${ERR_FILE}
   sleep 0.5
 done
