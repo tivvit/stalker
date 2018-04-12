c7ea60aee4cb963505d493ebfafaf3a343ba2760
Vit Listik <tivvit@seznam.cz>
2018-04-12 12:12:33 +0200

    idletime on mac

--- stalker_mac.sh
+++ stalker_mac.sh
@@ -10,6 +10,7 @@ fi
 while true; do
   FILE=${STALKER_DIR}`date +%y-%m-%d`.${PC}.log
   ERR_FILE=${STALKER_DIR}`date +%y-%m-%d`.${PC}.err.log
-  /Users/vit.listik/git/stalker/active-win >> ${FILE} 2> ${ERR_FILE}
+  IDLETIME=`ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print int($NF/1000000)}'`
+  /Users/vit.listik/git/stalker/active-win | /usr/local/bin/jq '. + {"idletime": '"${IDLETIME}"'}' -c >> ${FILE} 2> ${ERR_FILE}
   sleep 0.5
 done
