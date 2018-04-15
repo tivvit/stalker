d74482dd23bfb4bffb78071518f78e2483a35534
Vit Listik <tivvit@seznam.cz>
2018-04-15 16:06:28 +0200

    groups

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -3,6 +3,7 @@ var selectedItems = [];
 // todo show and hide controls
 
 const selectable = new Selectable({
+    appendTo: document.getElementById("selectable"),
     filter: ".record",
     toggle: true,
     classes: {
@@ -13,35 +14,142 @@ const selectable = new Selectable({
 
 selectable.on("selectable.select", function (selected) {
     var pos = selectedItems.indexOf(selected.node.id);
-    if (pos == -1) {
+    if (pos === -1) {
         selectedItems.push(selected.node.id);
     }
-    console.log(selectedItems)
 });
 
 selectable.on("selectable.unselect", function (unselected) {
     var pos = selectedItems.indexOf(unselected.node.id);
-    if (pos != -1) {
+    if (pos !== -1) {
         selectedItems.splice(pos);
     }
-    console.log(selectedItems)
 });
 
 document.getElementById("hide").onclick = function (ev) {
-  selectedItems.forEach(function (value) {
-      document.getElementById(value).classList.add("hidden");
-  });
-  selectedItems = [];
+    selectedItems.forEach(function (value) {
+        var elem = document.getElementById(value);
+        elem.classList.add("hidden");
+        elem.classList.remove("active")
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/hide", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status !== 200) {
+            console.log(xhr.status, xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({
+        "date": document.getElementById("date").value,
+        "items": selectedItems
+    });
+    xhr.send(data);
+    selectedItems = [];
 };
 
-// document.querySelectorAll(".record").forEach(function (item) {
-//     item.onclick = function (elem) {
-//         if (selected.indexOf(this.id) != -1) {
-//             selected.splice(selected.indexOf(this.id));
-//         } else {
-//             selected.push(this.id);
-//         }
-//         this.classList.toggle('active');
-//         console.log(selected)
-//     };
-// });
+document.getElementById("clear").onclick = function (ev) {
+    clear();
+};
+
+function clear() {
+    selectedItems.forEach(function (value) {
+        var elem = document.getElementById(value);
+        elem.classList.remove("active")
+    });
+    document.querySelectorAll(".active").forEach(function (value) {
+        value.classList.remove("active")
+    });
+    selectedItems = [];
+}
+
+document.getElementById("all").onclick = function (ev) {
+    selectedItems = [];
+    document.querySelectorAll(".record").forEach(function (value) {
+        if (value.offsetParent !== null) {
+            value.classList.add("active");
+            selectedItems.push(value.id);
+        }
+    });
+};
+
+document.getElementById("tag").onclick = function (ev) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/tags", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status !== 200) {
+            console.log(xhr.status, xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({
+        "date": document.getElementById("date").value,
+        "items": selectedItems,
+        "tags": document.getElementById("input").value.split(",")
+    });
+    xhr.send(data);
+    clear();
+    document.getElementById("input").value = "";
+    location.reload();
+};
+
+document.getElementById("describe").onclick = function (ev) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/describe", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status !== 200) {
+            console.log(xhr.status, xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({
+        "date": document.getElementById("date").value,
+        "items": selectedItems,
+        "description": document.getElementById("input").value
+    });
+    xhr.send(data);
+    clear();
+    document.getElementById("input").value = "";
+    location.reload();
+};
+
+var options = {
+    valueNames: [
+        'name',
+        'start',
+        {name: 'duration', attr: 'data-duration'},
+        'source',
+        'tags'
+    ]
+};
+
+var hackerList = new List('filter', options);
+
+document.getElementById("lg-filt").addEventListener("change", function (el) {
+    var filt = parseFloat(this.value);
+    hackerList.filter(function (i) {
+        if (isNaN(filt)) {
+            return true;
+        }
+        return i.values().duration > filt;
+    });
+});
+
+
+document.getElementById("sm-filt").addEventListener("change", function (el) {
+    var filt = parseFloat(this.value);
+    hackerList.filter(function (i) {
+        if (isNaN(filt)) {
+            return true;
+        }
+        return i.values().duration < filt;
+    });
+});
+
+document.querySelectorAll(".group_parent").forEach(function (value) {
+    value.addEventListener("click", function (el) {
+        console.log(this.parentElement.querySelectorAll(".group").forEach(function (value2) {
+            value2.classList.toggle("hidden");
+        }))
+    });
+});
--- web/web.py
+++ web/web.py
@@ -1,15 +1,19 @@
 import datetime
+import json
 import os
 
 from flask import Flask
+from flask import request
 from flask import render_template
 
+import analysis
 from analysis.analyze_day import human_time_diff
 from analysis.analyze_day import load_stream
 from analysis.analyze_day import process_stream
 from analysis.analyze_day import get_name
 
 app = Flask(__name__)
+data_path = "/data/"
 
 
 @app.template_filter()
@@ -19,29 +23,181 @@ def timediff(diff):
 
 @app.route("/")
 def main():
-    date = "18-04-14"
-    day_filename = os.path.join("/data/", date)
+    now = datetime.datetime.now()
+    # working over midnight - want to see "today"
+    if now.time() < analysis.config.morning:
+        now -= datetime.timedelta(days=1)
+    date = datetime.datetime.strftime(now, "%y-%m-%d")
+    day_filename = os.path.join(data_path, date)
     stream = load_stream(day_filename)
     next_day = datetime.datetime.strptime(date, "%y-%m-%d") + \
                datetime.timedelta(days=1)
-    day_filename = os.path.join("/data/",
+    day_filename = os.path.join(data_path,
                                 next_day.strftime("%y-%m-%d"))
     stream += load_stream(day_filename, only_morning=True)
+    datetime.timedelta().total_seconds()
     if not stream:
         # todo
         print("No data found")
         return
     times = process_stream(stream)
+    append_metadata(date, times)
+    times = create_groups(times)
     times.sort(key=lambda x: x["start"])
     # todo detect sources
     # times = [i for i in times if i["duration"] > datetime.timedelta(seconds=2)]
     for i in times:
         i.update({
-            "name": get_name(i["item"]),
-            "id": "{}-{}".format(i.get("source", "Unknown "),
-                                 i["start"].timestamp()),
+            "name": get_name(i["item"]) if "group" not in i else i["name"],
+            "id": "{}$-${}".format(i.get("source", "Unknown "),
+                                   i["start"].timestamp()),
         })
-    return render_template('index.html', data=times)
+    # todo hotfix
+    for i in times:
+        if "group" in i:
+            for g in i["group"]:
+                g.update({
+                    "name": get_name(g["item"]),
+                    "id": "{}$-${}".format(g.get("source", "Unknown "),
+                                           g["start"].timestamp()),
+                })
+    return render_template('index.html', data=times, date=date)
+
+
+def append_metadata(date, times):
+    filename = os.path.join(data_path,
+                            "{}{}".format(date, ".metadata.json"))
+    if not os.path.exists(filename):
+        return
+    metadata = json.load(open(filename, "r"))
+    for i in times:
+        src = i.get("source", "Unknown")
+        if src not in metadata:
+            continue
+        timestamp = str(i["start"].timestamp())
+        if timestamp in metadata[src]:
+            i.update(metadata[src][timestamp])
+
+
+def create_groups(times):
+    last_desc = None
+    out = []
+    group = []
+    for i in times:
+        if "description" not in i and not last_desc:
+            out.append(i)
+        elif "description" not in i and last_desc:
+            s = datetime.timedelta()
+            for g in group:
+                s += g["duration"]
+            out.append({
+                "start": group[0]["start"],
+                "duration": s,
+                "name": last_desc,
+                "group": group,
+            })
+            last_desc = None
+            group = []
+            out.append(i)
+        elif "description" in i and not last_desc:
+            last_desc = i["description"]
+            group = []
+            group.append(i)
+        elif i["description"] != last_desc:
+            s = datetime.timedelta()
+            for g in group:
+                s += g["duration"]
+            out.append({
+                "start": group[0]["start"],
+                "duration": s,
+                "name": last_desc,
+                "group": group,
+            })
+            last_desc = i["description"]
+            group = []
+            group.append(i)
+        else:
+            group.append(i)
+    if group:
+        s = datetime.timedelta()
+        for g in group:
+            s += g["duration"]
+        out.append({
+            "start": group[0]["start"],
+            "duration": s,
+            "name": last_desc,
+            "group": group,
+        })
+    return out
+
+
+@app.route("/hide", methods=["POST"])
+def hide():
+    data = request.json
+    filename = os.path.join(data_path,
+                            "{}{}".format(data["date"], ".metadata.json"))
+    metadata = {}
+    if os.path.exists(filename):
+        metadata = json.load(open(filename, "r"))
+    for i in data["items"]:
+        source, timestamp = i.split("$-$")
+        if source not in metadata:
+            metadata[source] = {}
+        if timestamp not in metadata[source]:
+            metadata[source][timestamp] = {}
+        metadata[source][timestamp].update({
+            "hidden": True,
+        })
+    json.dump(metadata, open(filename, "w"))
+    return ""
+
+
+@app.route("/tags", methods=["POST"])
+def tag():
+    data = request.json
+    filename = os.path.join(data_path,
+                            "{}{}".format(data["date"], ".metadata.json"))
+    metadata = {}
+    if os.path.exists(filename):
+        metadata = json.load(open(filename, "r"))
+    for i in data["items"]:
+        source, timestamp = i.split("$-$")
+        if source not in metadata:
+            metadata[source] = {}
+        if timestamp not in metadata[source]:
+            metadata[source][timestamp] = {}
+        tags = set(metadata[source][timestamp].get("tags", []))
+        for t in data.get("tags", []):
+            if t.startswith("-"):
+                e = t[1:]
+                if e in tags:
+                    tags.remove(e)
+            else:
+                tags.add(t)
+        metadata[source][timestamp]["tags"] = list(tags)
+    json.dump(metadata, open(filename, "w"))
+    return ""
+
+
+@app.route("/describe", methods=["POST"])
+def describe():
+    data = request.json
+    filename = os.path.join(data_path,
+                            "{}{}".format(data["date"], ".metadata.json"))
+    metadata = {}
+    if os.path.exists(filename):
+        metadata = json.load(open(filename, "r"))
+    for i in data["items"]:
+        source, timestamp = i.split("$-$")
+        if source not in metadata:
+            metadata[source] = {}
+        if timestamp not in metadata[source]:
+            metadata[source][timestamp] = {}
+        metadata[source][timestamp].update({
+            "description": data["description"],
+        })
+    json.dump(metadata, open(filename, "w"))
+    return ""
 
 
 if __name__ == "__main__":
